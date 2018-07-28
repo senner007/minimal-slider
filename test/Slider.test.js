8a16c13f08b9c01ab5299f2e4f0f504ff7487ab7
senner007 <nielshtg@hotmail.com>
2018-07-28 17:43:14 +0200

    rename test

--- test/Slider.test.js
+++ test/Slider.test.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 function createDOM () {
 
     return '<div id="container">'    +
-        '<div style="width: 400px;" class="margin-border">'  +
+        '<div style="width: 400px;" class="test-full-page">'  +
             '<ul>'+
                 '<li style="width: 400px; position: absolute;">Page 1</li>'+
                 '<li style="width: 400px; position: absolute;">Page 2</li>'+
@@ -18,7 +18,7 @@ function createDOM () {
 function setSlider () {
     
     return Slider({
-        ul: $('.margin-border').find('ul'),
+        ul: $('.test-full-page').find('ul'),
         infiniteScroll: true,
         touchDrag: true
     });
@@ -28,35 +28,35 @@ test('getState should return 1 on init', () => {
 
     // Set up our document body
     document.body.innerHTML = createDOM(); 
-    var moveMarginBorder = setSlider();
+    var testFullPage = setSlider();
 
-    expect(moveMarginBorder.getState()).toEqual(1);
+    expect(testFullPage.getState()).toEqual(1);
 });
 
 test('lis should align correctly', () => {
 
     // Set up our document body
     document.body.innerHTML = createDOM();
-    var moveMarginBorder = setSlider();
+    var testFullPage = setSlider();
 
     expect(window.getComputedStyle($('ul').children()[0])['width']).toEqual('400px');
     expect($('li')[0].style.width).toEqual('400px');
 
-    expect( $('.margin-border').find('li').eq(0).css('left') ).toEqual('-800px');
-    expect( $('.margin-border').find('li').eq(2).css('left') ).toEqual('0px');
-    expect( $('.margin-border').find('li').eq(8).css('left') ).toEqual('2400px');
+    expect( $('.test-full-page').find('li').eq(0).css('left') ).toEqual('-800px');
+    expect( $('.test-full-page').find('li').eq(2).css('left') ).toEqual('0px');
+    expect( $('.test-full-page').find('li').eq(8).css('left') ).toEqual('2400px');
 });
 
 test('should jump to 5 from 1 when infiniteScroll', async () => {
 
     document.body.innerHTML = createDOM();
-    var moveMarginBorder = setSlider();
+    var testFullPage = setSlider();
  
-    moveMarginBorder.moveLeft();
-    expect(moveMarginBorder.getState()).toEqual(5);
+    testFullPage.moveLeft();
+    expect(testFullPage.getState()).toEqual(5);
     // wait for move callback
     await new Promise(resolve => setTimeout(resolve, 30));
-    expect(moveMarginBorder._getTransformState()).toEqual(-1600);
+    expect(testFullPage._getTransformState()).toEqual(-1600);
     
 
 });
@@ -64,14 +64,14 @@ test('should jump to 5 from 1 when infiniteScroll', async () => {
 test('should jump to 1 from 5 when infiniteScroll', async () => {
 
     document.body.innerHTML = createDOM();
-    var moveMarginBorder = setSlider();
+    var testFullPage = setSlider();
  
-    moveMarginBorder.moveTo(5);
-    moveMarginBorder.moveRight();
-    expect(moveMarginBorder.getState()).toEqual(1);
+    testFullPage.moveTo(5);
+    testFullPage.moveRight();
+    expect(testFullPage.getState()).toEqual(1);
     // wait for moveMe callback
     await new Promise(resolve => setTimeout(resolve, 30));
-    expect(moveMarginBorder._getTransformState()).toEqual(0);
+    expect(testFullPage._getTransformState()).toEqual(0);
     
 
 });
