60a0b0f0b5b8033ac37803cb352c42aa2dd5cb7d
senner007 <nielshtg@hotmail.com>
2018-07-29 13:44:10 +0200

    add margin tests

--- test/Slider.test.js
+++ test/Slider.test.js
@@ -1,39 +1,41 @@
 'use strict';
-import {Slider} from '../src/Slider';
+import {
+    Slider
+} from '../src/Slider';
 import $ from 'jquery';
-function createDOM () {
 
-    return '<div id="container">'    +
-        '<div style="width: 400px;" class="test-full-page">'  +
-            '<ul>'+
-                '<li style="width: 400px; position: absolute;">Page 1</li>'+
-                '<li style="width: 400px; position: absolute;">Page 2</li>'+
-                '<li style="width: 400px; position: absolute;">Page 3</li>'+
-                '<li style="width: 400px; position: absolute;">Page 4</li>'+
-                '<li style="width: 400px; position: absolute;">Page 5</li>'+
-            '</ul>'+
-        '</div>'+
-    '</div>';
+function createDOM(width = '400px', marginLeft = '0px', marginRight = '0px') {
+
+    return `<div id="container">    
+                <div style="width: ${width};" class="test-full-page"> 
+                    <ul>
+                        <li style="width: 400px; position: absolute; margin-left: ${marginLeft}; margin-right: ${marginRight};">Page 1</li>
+                        <li style="width: 400px; position: absolute; margin-left: ${marginLeft}; margin-right: ${marginRight};">Page 2</li>
+                        <li style="width: 400px; position: absolute; margin-left: ${marginLeft}; margin-right: ${marginRight};">Page 3</li>
+                        <li style="width: 400px; position: absolute; margin-left: ${marginLeft}; margin-right: ${marginRight};">Page 4</li>
+                        <li style="width: 400px; position: absolute; margin-left: ${marginLeft}; margin-right: ${marginRight};">Page 5</li>
+                    </ul>
+                </div>
+            </div>`;
 }
-function setSlider () {
-    
-    return Slider({
-        ul: $('.test-full-page').find('ul'),
-        infiniteScroll: true,
-        touchDrag: true
-    });
-}
+
+const setSlider = () => Slider({
+    ul: $('.test-full-page').find('ul'),
+    infiniteScroll: true,
+    touchDrag: true
+});
+
 
 test('getState should return 1 on init', () => {
 
     // Set up our document body
-    document.body.innerHTML = createDOM(); 
+    document.body.innerHTML = createDOM();
     var testFullPage = setSlider();
 
     expect(testFullPage.getState()).toEqual(1);
 });
 
-test('lis should align correctly', () => {
+test('lis should align correctly, no margin', () => {
 
     // Set up our document body
     document.body.innerHTML = createDOM();
@@ -41,23 +43,53 @@ test('lis should align correctly', () => {
 
     expect(window.getComputedStyle($('ul').children()[0])['width']).toEqual('400px');
     expect($('li')[0].style.width).toEqual('400px');
+    expect($('li').outerWidth(true)).toEqual(400);
 
-    expect( $('.test-full-page').find('li').eq(0).css('left') ).toEqual('-800px');
-    expect( $('.test-full-page').find('li').eq(2).css('left') ).toEqual('0px');
-    expect( $('.test-full-page').find('li').eq(8).css('left') ).toEqual('2400px');
+    expect($('.test-full-page').find('li').eq(0).css('left')).toEqual('-800px');
+    expect($('.test-full-page').find('li').eq(2).css('left')).toEqual('0px');
+    expect($('.test-full-page').find('li').eq(8).css('left')).toEqual('2400px');
+});
+
+test('lis should align correctly, left margin', () => {
+
+    // Set up our document body
+    document.body.innerHTML = createDOM('410px', '10px');
+    var testFullPage = setSlider();
+
+    expect(window.getComputedStyle($('ul').children()[0])['width']).toEqual('400px');
+    expect($('li')[0].style.width).toEqual('400px');
+    expect($('li').outerWidth(true)).toEqual(410);
+
+    expect($('.test-full-page').find('li').eq(0).css('left')).toEqual('-820px');
+    expect($('.test-full-page').find('li').eq(2).css('left')).toEqual('0px');
+    expect($('.test-full-page').find('li').eq(8).css('left')).toEqual('2460px');
+});
+
+test('lis should align correctly, left margin, right margin', () => {
+
+    // Set up our document body
+    document.body.innerHTML = createDOM('410px', '5px', '5px');
+    var testFullPage = setSlider();
+
+    expect(window.getComputedStyle($('ul').children()[0])['width']).toEqual('400px');
+    expect($('li')[0].style.width).toEqual('400px');
+    expect($('li').outerWidth(true)).toEqual(410);
+
+    expect($('.test-full-page').find('li').eq(0).css('left')).toEqual('-820px');
+    expect($('.test-full-page').find('li').eq(2).css('left')).toEqual('0px');
+    expect($('.test-full-page').find('li').eq(8).css('left')).toEqual('2460px');
 });
 
 test('should jump to 5 from 1 when infiniteScroll', async () => {
 
     document.body.innerHTML = createDOM();
     var testFullPage = setSlider();
- 
+
     testFullPage.moveLeft();
     expect(testFullPage.getState()).toEqual(5);
     // wait for move callback
     await new Promise(resolve => setTimeout(resolve, 30));
     expect(testFullPage._getTransformState()).toEqual(-1600);
-    
 
 });
 
@@ -65,13 +97,12 @@ test('should jump to 1 from 5 when infiniteScroll', async () => {
 
     document.body.innerHTML = createDOM();
     var testFullPage = setSlider();
- 
+
     testFullPage.moveTo(5);
     testFullPage.moveRight();
     expect(testFullPage.getState()).toEqual(1);
     // wait for moveMe callback
     await new Promise(resolve => setTimeout(resolve, 30));
     expect(testFullPage._getTransformState()).toEqual(0);
-    
 
 });
