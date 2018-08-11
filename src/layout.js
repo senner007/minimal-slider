aa3342d5acfe1001add07b71be0a5a2280f6a049
senner007 <nielshtg@hotmail.com>
2018-08-11 14:58:56 +0200

    layout refactor continued

--- src/layout.js
+++ src/layout.js
@@ -1,80 +1,64 @@
+import {cloneCurry, getOrigStyles} from './layoutHelpers';
+
 export default function Layout (list, infiniteScroll) {
 
     var liOuter,
         minWidth = parseInt(list.find('li').css('min-width')),
         listLiJs = list[0].children; // vanilla js object
 
-    // hack to retrieve css set unit value(%|px)
-    listLiJs[0].style.display = 'none';
-    var origStyle = window.getComputedStyle(listLiJs[0]);
-    var origStyleWidth = origStyle['width'];
-    listLiJs[0].style.display = 'block';
-
-    var marginBorder = parseInt(origStyle.marginLeft) + parseInt(origStyle.marginRight) + parseInt(origStyle.borderLeftWidth) + parseInt(origStyle.borderRightWidth);
-
-    function createClones (arr, position) {   
-            return arr.map((c) => {
-                var clone = listLiJs[c -1].cloneNode(true)
-                clone.classList.add('clone');    
-                clone.style.left = parseInt(clone.style.left) + listLiJs.length * position + "px";
-                return clone;           
-            }); 
-    }
-
-    function addClones(clones, addfn) {
-        clones.forEach((clone) => {          
-            list[addfn](clone);                                     
-         });
-    }
+    var orig = getOrigStyles(listLiJs[0]);
 
     return {
         get liOuter() {
             return Math.round(liOuter);
         },
-        setStyles: function (elems) {
+        setStyles: function () {
      
             var ulParentwidth = Math.round(list.parent().outerWidth(true));
             // scale according to width percentage if ulParentwidth is above 2 times the width of the li min-width
-            if (origStyleWidth.indexOf('%') != -1 && ulParentwidth / 2 > minWidth) {
-                liOuter = parseInt(origStyleWidth.replace('%', '') / 100 * ulParentwidth);
-            } else {
-                liOuter = parseInt(list.find('li').outerWidth(true));
-            }
 
+            liOuter = (function () {
+                if (orig.width.indexOf('%') != -1 && ulParentwidth / 2 > minWidth) {
+                    return parseInt(orig.width.replace('%', '') / 100 * ulParentwidth);
+                }          
+                return parseInt(list.find('li').outerWidth(true));
+            }());
+
+          
             (function (index = 0) {
-                for (let i = 0; i < listLiJs.length; i++) {
-                    if (!listLiJs[i].classList.contains('clone')) {
-                        listLiJs[i].style.width = liOuter - marginBorder + 'px';
-                        listLiJs[i].style.left = (liOuter) * index++ + (ulParentwidth / 2 - (liOuter / 2)) + 'px';
-                    }
-                }
+                Array.from(listLiJs).forEach((v,i) => {
+                     if (!v.classList.contains('clone')) {
+                        v.style.width = liOuter - orig.marginBorder + 'px';
+                        v.style.left =  liOuter * index++ + (ulParentwidth / 2 - (liOuter / 2)) + 'px';
+                     }
+                })
             })()
-
-            // append clones to appear infinite
+         
             if (infiniteScroll) {
-                //  list[0].querySelectorAll('.clone') remove
-                // Array.prototype.forEach.call(list[0].querySelectorAll('.clone'), function (node) {
-                //     node.parentNode.removeChild(node);
-                // });
 
-
+                // for (let li of list[0].querySelectorAll('.clone')) {
+                //     li.remove();
+                // }
                 // this causes babel to inject symbol and iterator code which requires import 'babel-polyfill' for ie 
                 // https://babeljs.io/docs/en/babel-plugin-transform-es2015-for-of
                 // https://daverupert.com/2017/10/for-of-loops-are-bad/
                 // TODO : add to es-lint : no-for-of-loops
-                // for (let li of list[0].querySelectorAll('.clone')) {
-                //     li.remove();
-                // }
+              
+
                 (function deleteClones () {
-                    var clones = document.getElementsByClassName('clone');
-                    while(clones[0]) {
-                        clones[0].parentNode.removeChild(clones[0]);
-                    }
+                    Array.from(listLiJs).forEach(v => {
+                        if (v.classList.contains('clone')) { 
+                            v.parentNode.removeChild(v)
+                        }
+                    })    
                 }());
-                
-                addClones(createClones([1,2], liOuter), 'append');
-                addClones(createClones([listLiJs.length, listLiJs.length -1], -liOuter), 'prepend')
-       
+               
+                (function addClones (elems = listLiJs.length) {
+                    var createClones  = cloneCurry(elems, listLiJs);
+                    createClones([1,2], liOuter).addClones();
+                    createClones([elems , elems -1], -liOuter).prependClones();
+                 }());              
+
             }
         },
         addRemove : function (position, elem) {
---
+++ src/layoutHelpers.js
@@ -0,0 +1,40 @@
+function cloneCurry (elems, list) { 
+         
+    return function createClones (arr, factor) {   
+
+        arr = arr.map((c) => {
+            var clone = list[c -1].cloneNode(true)
+            clone.classList.add('clone');    
+            clone.style.left = parseInt(clone.style.left) + elems * factor + "px";
+            return clone;           
+        }); 
+        
+        return {
+            addClones: function() {
+                console.log(arr)
+                arr.forEach((clone) => list[0].parentNode.appendChild(clone));
+            },
+            prependClones: function () {
+                arr.forEach((clone) => list[0].parentNode.insertBefore(clone, list[0]));
+            }
+        }
+    }
+}
+
+function getOrigStyles (liElem) { 
+
+    liElem.style.display = 'none';
+    var origStyle = window.getComputedStyle(liElem);
+    var origWidth = origStyle['width'];
+    liElem.style.display = 'block';
+
+    return {
+        width : origWidth,
+        marginBorder : parseInt(origStyle.marginLeft) + parseInt(origStyle.marginRight) + parseInt(origStyle.borderLeftWidth) + parseInt(origStyle.borderRightWidth)
+    }
+
+}
+
+
+
+export {cloneCurry, getOrigStyles}
