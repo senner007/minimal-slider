e2a9fa2b7cd015a74ccdb2d5470e6fa417ad6a3b
senner007 <nielshtg@hotmail.com>
2018-08-13 15:34:41 +0200

    rename var

--- src/layout.js
+++ src/layout.js
@@ -3,17 +3,17 @@ import {cloneCurry, getOrigStyles, outerWidth} from './layoutHelpers';
 export default function Layout (listLiJs, infiniteScroll) {
 
     var liOuter,
-        ulWidth,
+        ulParentWidth,
         orig = getOrigStyles(listLiJs[0]);
 
 
     function setWidths () {
-        ulWidth = outerWidth(listLiJs[0].parentNode);
+        ulParentWidth = outerWidth(listLiJs[0].parentNode);
 
-        // scale according to width percentage if ulWidth is above 2 times the width of the li min-width
+        // scale according to width percentage if ulParentWidth is above 2 times the width of the li min-width
         liOuter = (function () {
-            if (orig.width.indexOf('%') != -1 && ulWidth / 2 > orig.minWidth) {
-                return parseInt(orig.width.replace('%', '') / 100 * ulWidth);
+            if (orig.width.indexOf('%') != -1 && ulParentWidth / 2 > orig.minWidth) {
+                return parseInt(orig.width.replace('%', '') / 100 * ulParentWidth);
             }
             return outerWidth(listLiJs[0]);
         }());
@@ -23,10 +23,12 @@ export default function Layout (listLiJs, infiniteScroll) {
         listLiJs.toArray().forEach((v) => {
              if (!v.classList.contains('clone')) {
                 v.style.width = liOuter - orig.marginBorder + 'px';
-                v.style.left =  liOuter * index++ + (ulWidth / 2 - (liOuter / 2)) + 'px';
+                v.style.left =  liOuter * index++ + (ulParentWidth / 2 - (liOuter / 2)) + 'px';
              } else {
                 // remove clones
                 fn(v)
+
+                
              }
         })
     };
@@ -51,13 +53,12 @@ export default function Layout (listLiJs, infiniteScroll) {
                 // https://babeljs.io/docs/en/babel-plugin-transform-es2015-for-of
                 // https://daverupert.com/2017/10/for-of-loops-are-bad/
                 // TODO : add to es-lint : no-for-of-loops
-
+             
                 (function addClones (elems = listLiJs.length) {
                     var createClones  = cloneCurry(elems, listLiJs);
                     createClones([1,2], liOuter).addClones();
                     createClones([elems , elems -1], -liOuter).prependClones();
-                 }());
-
+                }());
             }
         },
         addRemove : function (position, elem) {
